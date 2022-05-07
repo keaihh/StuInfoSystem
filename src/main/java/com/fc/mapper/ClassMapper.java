e9f121ee8ae7ad4c26c417bb6d83deb37c1c6d0b
keaihh <3334273598@qq.com>
2022-05-07 15:59:16 +0800

    Initial commit

---
+++ src/main/java/com/fc/mapper/ClassMapper.java
@@ -0,0 +1,27 @@
+package com.fc.mapper;
+
+import com.fc.bean.Classes;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+
+@Mapper
+public interface ClassMapper {
+
+    @Select("select * from class")
+    List<Classes> selectAllClass();
+
+    @Select("select * from class where class_id=#{classId}")
+    Classes selectClassById(String classId);
+
+
+    @Delete("DELETE from class where class_id=#{classId}")
+    int delectClassById(String classId);
+
+    @Insert("insert into class values(#{classId},#{className})")
+    int insertClass(Classes clas);
+
+    @Select("select * from class where class_name=#{className}")
+    Classes selectClassByName(String className);
+}
